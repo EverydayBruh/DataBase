dbb86c98d25e591b3045bc281b240686faefb4d9
EverydayBruh <roliki2222222@gmail.com>
2024-06-01 21:09:20 +0300

    Create init_2.sql

---
+++ Lab1/scripts/init_2.sql
@@ -0,0 +1,68 @@
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    location TEXT NOT NULL
+);
+
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    purchase_price REAL NOT NULL,
+    sale_price REAL NOT NULL,
+    supplier_id INTEGER,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+);
+
+CREATE TABLE suppliers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    contact_info TEXT NOT NULL
+);
+
+CREATE TABLE sales_channels (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    discount_factor REAL NOT NULL
+);
+
+CREATE TABLE sales (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER,
+    seller_id INTEGER,
+    customer_id INTEGER,
+    sale_date DATE NOT NULL,
+    quantity INTEGER NOT NULL,
+    discount REAL,
+    sales_channel_id INTEGER,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (seller_id) REFERENCES sellers(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (sales_channel_id) REFERENCES sales_channels(id)
+);
+
+CREATE TABLE sellers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    discount_card BOOLEAN NOT NULL,
+    discount REAL
+);
+
+CREATE TABLE inventory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER,
+    department_id INTEGER,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
